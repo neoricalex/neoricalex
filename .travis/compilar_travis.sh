7e6eefeb9d7688c592b4e69b9219975d46a2ee61
neoricalex <neo.webmaster.2@gmail.com>
2021-03-18 11:33:09 -0300

    Atualizar Travis

--- .travis/compilar_travis.sh
+++ .travis/compilar_travis.sh
@@ -3,6 +3,7 @@
 #sudo VAGRANT_VAGRANTFILE=src/vps/.travis/Vagrantfile vagrant up --provider=libvirt
 #- sudo VAGRANT_VAGRANTFILE=src/vps/.travis/Vagrantfile vagrant ssh -- -t 'sudo apt update; sudo apt install git -y; git clone https://github.com/neoricalex/neoricalex.git; cd neoricalex; bash shell'
 
+vagrant destroy --force
 sudo vagrant up --provider=libvirt
 sudo vagrant ssh <<EOF
 #!/bin/bash
