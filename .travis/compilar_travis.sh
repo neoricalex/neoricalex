d91ce0c142183fbccc6258e71c276cbc261e583c
neoricalex <neo.webmaster.2@gmail.com>
2021-03-18 12:31:50 -0300

    Atualizar Travis

--- .travis/compilar_travis.sh
+++ .travis/compilar_travis.sh
@@ -8,16 +8,15 @@ sudo vagrant up --provider=libvirt
 sudo vagrant ssh <<EOF
 #!/bin/bash
 
-SETAR_HOSTNAME="vps-teste"
-sudo echo $SETAR_HOSTNAME > /proc/sys/kernel/hostname
-sudo sed -i 's/127.0.1.1.*/127.0.1.1\t'"$SETAR_HOSTNAME"'/g' /etc/hosts
-sudo echo $SETAR_HOSTNAME > /etc/hostname
-sudo service hostname start
-#su $SUDO_USER -c "xauth add $(xauth list | sed 's/^.*\//'"$SETAR_HOSTNAME"'\//g' | awk 'NR==1 {sub($1,"\"&\""); print}')"
-
-echo "$USER@$HOSTNAME"
 sudo apt update
-sudo apt install git -y
+sudo apt-get install linux-generic linux-headers-`uname -r` ubuntu-minimal -y
+
+sudo apt-get install -y build-essential checkinstall libreadline-gplv2-dev \
+    libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev \
+    libbz2-dev libffi-dev python3-pip unzip lsb-release software-properties-common \
+    curl wget git rsync python-dev python3-venv xrdp libterm-readline-gnu-perl \
+    dkms git
+
 git clone https://github.com/neoricalex/neoricalex.git
 cd neoricalex
 bash shell
