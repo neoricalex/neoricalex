e6af1e58a632f1943c44dc7d531feb5e758813f0
neoricalex <neo.webmaster.2@gmail.com>
2021-03-18 12:48:47 -0300

    Atualizar Travis

--- .travis/compilar_travis.sh
+++ .travis/compilar_travis.sh
@@ -2,6 +2,14 @@
 
 #sudo VAGRANT_VAGRANTFILE=src/vps/.travis/Vagrantfile vagrant up --provider=libvirt
 #- sudo VAGRANT_VAGRANTFILE=src/vps/.travis/Vagrantfile vagrant ssh -- -t 'sudo apt update; sudo apt install git -y; git clone https://github.com/neoricalex/neoricalex.git; cd neoricalex; bash shell'
+sudo apt update
+sudo apt-get install linux-generic linux-headers-`uname -r` ubuntu-minimal -y
+
+sudo apt-get install -y build-essential checkinstall libreadline-gplv2-dev \
+    libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev \
+    libbz2-dev libffi-dev python3-pip unzip lsb-release software-properties-common \
+    curl wget git rsync python-dev python3-venv xrdp libterm-readline-gnu-perl \
+    dkms git
 
 vagrant destroy --force
 sudo vagrant up --provider=libvirt
@@ -19,5 +27,6 @@ sudo apt-get install -y build-essential checkinstall libreadline-gplv2-dev \
 
 git clone https://github.com/neoricalex/neoricalex.git
 cd neoricalex
+echo "[DEBUG] Até aqui parece bom!
 bash shell
 EOF
