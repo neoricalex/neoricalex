2ac494cbf1758cc428e4ef8247851b91300f9bad
neoricalex <neo.webmaster.2@gmail.com>
2021-03-18 14:46:24 -0300

    Atualizar Travis

--- .travis/compilar_travis.sh
+++ .travis/compilar_travis.sh
@@ -9,7 +9,10 @@ sudo vagrant ssh <<EOF
 #!/bin/bash
 
 sudo apt update
-sudo apt install git -y
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+sudo apt-get install -y -q
+sudo apt-get install dialog apt-utils git -y
+
 git clone https://github.com/neoricalex/neoricalex.git
 
 echo "[DEBUG] Até aqui parece bom!"
