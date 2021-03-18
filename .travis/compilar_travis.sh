6ba21d597f7c6f42411e3d5e5ad0fcb5ce6cff84
neoricalex <neo.webmaster.2@gmail.com>
2021-03-18 12:09:29 -0300

    Atualizar Travis

--- .travis/compilar_travis.sh
+++ .travis/compilar_travis.sh
@@ -7,8 +7,14 @@ vagrant destroy --force
 sudo vagrant up --provider=libvirt
 sudo vagrant ssh <<EOF
 #!/bin/bash
-pwd
-ls
+
+SETAR_HOSTNAME="vps-teste"
+echo $SETAR_HOSTNAME > /proc/sys/kernel/hostname
+sed -i 's/127.0.1.1.*/127.0.1.1\t'"$SETAR_HOSTNAME"'/g' /etc/hosts
+echo $SETAR_HOSTNAME > /etc/hostname
+service hostname start
+#su $SUDO_USER -c "xauth add $(xauth list | sed 's/^.*\//'"$SETAR_HOSTNAME"'\//g' | awk 'NR==1 {sub($1,"\"&\""); print}')"
+
 echo "$USER@$HOSTNAME"
 sudo apt update
 sudo apt install git -y
