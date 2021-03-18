8e996d8e5c80166215f0c77425272bd763246646
neoricalex <neo.webmaster.2@gmail.com>
2021-03-18 16:33:12 -0300

    Atualizar Travis

--- src/vps/compilar.sh
+++ src/vps/compilar.sh
@@ -2,12 +2,30 @@
 
 # Iniciar o VPS
 # REF: https://github.com/neoricalex/baseimage-docker.git
-
-#vagrant destroy
-vagrant up
-vagrant ssh <<EOF
+compilar_local(){
+    #vagrant destroy
+    vagrant up
+    vagrant ssh <<EOF
 #!/bin/bash
 cd /vagrant
 make vps
 cd ..
 EOF
+}
+
+compilar_travis(){
+    vagrant destroy --force
+    vagrant up travis --provider=libvirt
+    vagrant ssh <<EOF
+#!/bin/bash
+echo "[DEBUG] Até aqui parece bom! [$USER@$HOSTNAME]"
+cd /vagrant
+make vps
+cd ..
+EOF
+}
+
+case $HOSTNAME in
+    (travis-job-*) compilar_travis;;
+    (*)   compilar_local;;
+esac
