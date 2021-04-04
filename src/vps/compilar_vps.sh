13450c3fec25fa734c73123afe2a91259c05e26d
neoricalex <neo.webmaster.2@gmail.com>
2021-04-04 01:14:39 -0300

    Atualização Automática via bash shell

--- src/vps/compilar_vps.sh
+++ src/vps/compilar_vps.sh
@@ -100,10 +100,9 @@ echo "==> Instalar paxotes extras..."
 sudo apt-get install -y build-essential checkinstall libreadline-gplv2-dev \
     libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev \
     libbz2-dev libffi-dev python3-pip unzip lsb-release software-properties-common \
-    curl wget git rsync python-dev python3-venv libterm-readline-gnu-perl \
-    dkms
+    curl wget git rsync python-dev python3-venv libterm-readline-gnu-perl 
 
-if ! command -v unzip &> /dev/null;
+if ! command -v xorriso &> /dev/null;
 then
 	echo "==> Instalar pacotes para a criação da imagem ISO..."
 	sudo apt install -y \
@@ -265,6 +264,24 @@ entrar_vps(){
 #!/bin/bash
 
 cd /neoricalex
+
+if ! command -v xorriso &> /dev/null;
+then
+	echo "==> Instalar pacotes para a criação da imagem ISO..."
+	sudo apt install -y \
+		binutils \
+		debootstrap \
+		squashfs-tools \
+		xorriso \
+		grub-pc-bin \
+		grub-efi-amd64-bin \
+		mtools \
+		whois \
+		jq \
+		moreutils \
+		make \
+		unzip
+fi
 
 if ! vagrant plugin list | grep "vagrant-libvirt" > /dev/null;
 then
