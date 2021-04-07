1c8dc596c9be5a9f9f1d44ec142178184df66b83
neoricalex <neo.webmaster.2@gmail.com>
2021-04-06 23:38:33 -0300

    Atualização Automática via bash shell

--- src/vps/compilar_vps.sh
+++ src/vps/compilar_vps.sh
@@ -98,6 +98,8 @@ fi
 
 vagrant destroy -f NFDOS
 vagrant box remove neoricalex/nfdos
+virsh vol-delete --pool default neoricalex-VAGRANTSLASH-nfdos_vagrant_box_image_0.img
+virsh vol-delete --pool default NEORICALEX_NFDOS-vdb.qcow2
 virsh vol-list default
 vagrant status
 exit
