eb3fbf410d251bd8171ab2b718cd6e559ee7b513
neoricalex <neo.webmaster.2@gmail.com>
2021-04-07 11:03:33 -0300

    Atualização Automática via bash shell

--- src/vps/compilar_vps.sh
+++ src/vps/compilar_vps.sh
@@ -90,10 +90,50 @@ then
 	rm packer_1.6.4_linux_amd64.zip
 fi
 
-vagrant destroy -f
-vagrant box remove neoricalex/nfdos
-virsh vol-delete --pool default neoricalex-VAGRANTSLASH-nfdos_vagrant_box_image_0.img
-virsh vol-delete --pool default NEORICALEX_NFDOS-vdb.qcow2
+if ! command -v kvm-ok &> /dev/null;
+then
+	echo "==> Instalar os pacotes do kvm"
+	sudo apt install -y qemu-system qemu qemu-kvm qemu-utils qemu-block-extra \
+						libvirt-daemon libvirt-daemon-system libvirt-clients \
+						cpu-checker libguestfs-tools libosinfo-bin \
+						bridge-utils dnsmasq-base ebtables libvirt-dev
+
+	sudo usermod -aG kvm vagrant
+	sudo usermod -aG libvirt vagrant
+
+	echo "==> Adicionar o grupo kvm"
+	sudo groupadd kvm
+
+	echo "==> Adicionar o usuário vagrant ao grupo kvm"
+	sudo usermod -aG kvm vagrant
+
+	echo "==> Adicionar o usuário vagrant ao grupo libvirt"
+	sudo usermod -aG libvirt vagrant
+
+	echo "==> Iniciar o serviço KVM de forma automática"
+	sudo systemctl start libvirtd
+	sudo systemctl enable --now libvirtd
+
+	echo "==> Reiniciar o serviço libvirt"
+	sudo systemctl restart libvirtd.service
+
+	echo "==> Habilitar o IPv4 e IPv6 forwarding"
+	sudo sed -i "/net.ipv4.ip_forward=1/ s/# *//" /etc/sysctl.conf
+	#sudo sed -i "/net.ipv6.conf.all.forwarding=1/ s/# *//" /etc/sysctl.conf
+
+	echo "==> Aplicar as mudanças"
+	sudo sysctl -p
+
+	sudo chown root:kvm /dev/kvm
+	sudo chmod -R 660 /dev/kvm
+	sudo udevadm control --reload-rules
+	sudo systemctl restart libvirtd
+fi
+
+#vagrant destroy -f
+#vagrant box remove neoricalex/nfdos
+#virsh vol-delete --pool default neoricalex-VAGRANTSLASH-nfdos_vagrant_box_image_0.img
+#virsh vol-delete --pool default NEORICALEX_NFDOS-vdb.qcow2
 
 echo "Compilando o NFDOS..."
 make nfdos
